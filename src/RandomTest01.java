0a099897aa80e1fa72bd9d9503568bd85d01c3fc
debajitjp <debajitjp@gmail.com>
2020-08-12 08:21:06 +0900

    Initial commit

---
+++ src/RandomTest01.java
@@ -0,0 +1,13 @@
+public class RandomTest01 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for(i=9; i>-1; i--){
+            System.out.print(i+" ");
+
+        }
+    }
+
+
+}
+
